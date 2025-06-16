b9075c524e12168f864e072d9cf1f5af3d65a8cc
Abdalrhman Ashraf <125604105+CtrlAbdoo@users.noreply.github.com>
2025-06-16 06:00:07 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -55,8 +55,8 @@ class Settings(BaseSettings):
             "http://127.0.0.1:5500",
             "http://localhost:8000",
             "http://localhost:8081",
-           "https://sha-bot.netlify.app/"
-            "https://sha-bot.netlify.app"
+           "https://sha-bot.netlify.app/",
+            "https://sha-bot.netlify.app",
             
         ]
     )
